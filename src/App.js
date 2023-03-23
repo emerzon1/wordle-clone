afb0c1d6f63fcfac53b0fe2daf32efd097eaebe9
EHS Coding Club <edgemonthsprogramming@gmail.com>
2023-03-23 15:42:53 -0400

    Work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React, {useState} from 'react'
+import WordContainer from './Components/wordContainer'
+
+const initialWordState = () => ([...Array(6)].map(x => [...Array(5)].map(x => ({letter: "", color: ""}))))
+
 function App() {
+  const [wordState, setWordState] = useState(initialWordState())
+  console.log(wordState)
   return (
-    <div>
-      Start
+    <div style={{width: "100%", height: "100%"}}>
+      <WordContainer wordState={wordState} />
     </div>
   );
 }
---
+++ src/Components/wordContainer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import "./wordContainer.css"
+
+const WordContainer = ({wordState}) => {
+    return <div className="container">
+        { wordState.map(row => {
+
+            return <div className="row">
+                {row.map(e => {
+                    return <div className="letter">
+
+                    </div>
+                })}
+            </div>
+        })
+        }
+    </div>
+}
+
+export default WordContainer;
